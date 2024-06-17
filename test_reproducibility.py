4e35e96b15c3ff6b8e5ed626ab7db684e7e47a43
Michał Czuba <michal.czuba.1995@gmail.com>
2024-06-17 07:49:37 -0400

    enhanced e2e test readability

--- test_reproducibility.py
+++ test_reproducibility.py
@@ -33,7 +33,7 @@ def compare_results(gt_dir: Path, test_dir: Path, csv_names: list[str]) -> None:
     for csv_name in csv_names:
         gt_df = pd.read_csv(gt_dir / csv_name, index_col=0)
         test_df = pd.read_csv(test_dir / csv_name, index_col=0)
-        pd.testing.assert_frame_equal(gt_df, test_df)
+        pd.testing.assert_frame_equal(gt_df, test_df, obj=csv_name)
         print(f"Test passed for {csv_name}")
 
 
