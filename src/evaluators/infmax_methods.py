47df9cb5a44d5f2cfe496c7e597ef05628b8973a
Michał Czuba <michal.czuba.1995@gmail.com>
2024-12-03 17:18:52 +1100

    modified GT so that it uses narrow params of ICM

--- src/evaluators/infmax_methods.py
+++ src/evaluators/infmax_methods.py
@@ -7,7 +7,7 @@ import network_diffusion as nd
 import pandas as pd
 
 from _data_set.nsl_data_utils.loaders.constants import (
-    ACTOR, EXPOSED, NETWORK, P, PEAK_INFECTED, PEAK_ITERATION, PROTOCOL, SIMULATION_LENGTH
+    ACTOR, AND, EXPOSED, NETWORK, OR, P, PEAK_INFECTED, PEAK_ITERATION, PROTOCOL, SIMULATION_LENGTH
 )
 from _data_set.nsl_data_utils.loaders.sp_loader import load_sp, _get_sp, _sort_csv_paths
 from _data_set.nsl_data_utils.loaders.centrality_loader import load_centralities
@@ -38,6 +38,8 @@ class CentralityChoice:
 
 class GroundTruth:
 
+    _VALID_ICM_PARAMS = {AND: {0.80, 0.85, 0.90, 0.95}, OR: {0.05, 0.10, 0.15, 0.20}}
+
     def __init__(self, nb_seeds: int, average_protocol: bool, average_p_value: bool) -> None:
         self.nb_seeds = nb_seeds
         self.average_protocol = average_protocol
@@ -46,34 +48,28 @@ class GroundTruth:
     @staticmethod
     def get_top_k(
         sp_raw: pd.DataFrame,
-        protocol: str | None,
-        p: float | None,
+        protocol: list[str],
+        p: list[float],
         budget: int
     ) -> list[str]:
         """
         Get actors that performed the best in given spreading contitions.
 
         :param sp_raw: DataFrame loaded with `load_sp` with spreading potentials for a given network
-        :param protocol: protocol of the multilayer ICM
-        :param p: probability of the multilayer ICM, if not provided it will be discarded in the
-            process of selecting top-k actors
+        :param protocol: a list of protocols to consider 
+        :param p: a list of probabilities to consider 
         :param budget: top-k actors to return, if not provided it will be discarded in the process
             of selecting top-k actors
         :return: IDs of actors that performed the best in given contidions
         """
-        if not protocol:
-            protocol = 0
-            sp_raw[PROTOCOL] = protocol
-        if not p:
-            p = 0
-            sp_raw[P] = p
-        sp_mean  = sp_raw.groupby(by=[NETWORK, PROTOCOL, ACTOR, P]).mean().reset_index()
-        sp_mean = sp_mean[(sp_mean[PROTOCOL] == protocol) & (sp_mean[P] == p)]
+        sp_filtered = sp_raw[sp_raw[PROTOCOL].isin(protocol) & sp_raw[P].isin(p)]
+        sp_filtered = sp_filtered.drop([P, PROTOCOL, NETWORK], axis=1)
+        sp_mean = sp_filtered.groupby(by=[ACTOR]).mean()
         sp_mean = sp_mean.sort_values(
             [EXPOSED, SIMULATION_LENGTH, PEAK_INFECTED, PEAK_ITERATION],
             ascending=[False, True, True, False]
         )
-        return sp_mean.iloc[:budget][ACTOR].tolist()
+        return sp_mean.iloc[:budget].index.tolist()
 
     def __call__(
             self,
@@ -83,13 +79,15 @@ class GroundTruth:
             p: float,
             **kwargs,
     ) -> list[str]:
+        assert p in self._VALID_ICM_PARAMS[protocol], "Eval. feasible only on narrow range of p!"
         if net_type == net_name:
             raw_sp = load_sp(net_name=net_type)[net_name]
         else:
             csv_paths = _sort_csv_paths(f"{net_type}/*.csv")
             raw_sp = _get_sp(csv_paths[net_name])
-        protocol = None if self.average_protocol else protocol
-        p = None if self.average_p_value else p
+        protocol = [OR, AND] if self.average_protocol else [protocol]
+        p = [p_val for proto in protocol for p_val in self._VALID_ICM_PARAMS[proto]] \
+            if self.average_p_value else [p]
         return self.get_top_k(sp_raw=raw_sp, budget=self.nb_seeds, protocol=protocol, p=p)
 
 
