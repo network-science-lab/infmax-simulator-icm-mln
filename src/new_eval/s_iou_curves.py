100f0c15ca9e83b796fe0a76b4be90c1acb291dd
Michał Czuba <michal.czuba.1995@gmail.com>
2025-03-17 19:47:06 +0000

    added draft of the visualisation

--- src/new_eval/s_iou_curves.py
+++ src/new_eval/s_iou_curves.py
@@ -1,37 +1,25 @@
-from matplotlib.figure import Figure
+from typing import Any
+
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import scipy.interpolate
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.figure import Figure
 
 
-def read_and_sort(df_path: str) -> pd.DataFrame:
-    df = pd.read_csv(df_path)
-    try: 
-        df["actor"] = df["Unnamed: 0"]
-        df = df.drop("Unnamed: 0", axis=1)
-    except:
-        ...
-    df = df.set_index("actor")
-    df = df.sort_values(
-        ["exposed", "simulation_length", "peak_infected", "peak_iteration"],
-        ascending=[False, True, True, False]
-    )
-    return df
+def acc(arr_y: list[Any], arr_yhat: list[list[Any]], cutoff: int) -> float:
+    y = set(arr_y[:cutoff])
+    yhs = [set(ayh[:cutoff]) for ayh in arr_yhat]
+    accs = [len(y.intersection(yh)) / cutoff for yh in yhs]
+    return np.mean(accs).item()
 
 
-def acc(df_y: pd.DataFrame, df_yhat: pd.DataFrame, cutoff: int) -> float:
-    y = set(df_y.iloc[:cutoff].index.values)
-    y_hat = set(df_yhat.iloc[:cutoff].index.values)
-    return len(y.intersection(y_hat)) / cutoff
-
-
-def cummulated_acc(df_y: pd.DataFrame, df_yhat: pd.DataFrame) -> np.array:
-    assert len(df_y) == len(df_yhat)
+def cummulated_acc(arr_y: list[Any], arr_yhat: list[list[Any]]) -> np.array:
+    assert all([len(arr_y) == len(ayh) for ayh in arr_yhat])
     accs = []
-    cutoffs = np.linspace(1, len(df_y), len(df_y), dtype=int)
+    cutoffs = np.linspace(1, len(arr_y), len(arr_y), dtype=int)
     for cutoff in cutoffs:
-        cutoff_acc = acc(df_y=df_y, df_yhat=df_yhat, cutoff=cutoff)
+        cutoff_acc = acc(arr_y=arr_y, arr_yhat=arr_yhat, cutoff=cutoff)
         accs.append(cutoff_acc)
     return np.array(accs)
 
@@ -58,21 +46,29 @@ def average_curves(
     return avg_curve
 
 
-def plot_accs(df_ys: list[pd.DataFrame], df_yhats: list[pd.DataFrame], df_names: list[str]) -> Figure:
+def plot_accs(accs: dict[str, list[float]],  plot_avg: bool = True) -> Figure:
     fig, ax = plt.subplots(nrows=1, ncols=1)
+    if plot_avg:
+        alpha = 0.2
+    else:
+        alpha = 0.6
 
     acc_yhats = []
-    for df_y, df_yhat, name in zip(df_ys, df_yhats, df_names):
-        acc_yhat = cummulated_acc(df_y=df_y, df_yhat=df_yhat)
-        cutoffs_yhat = random_acc(len(df_y))
-        ax.plot(cutoffs_yhat, acc_yhat, label=f"{name}", alpha=0.2)
+    for name, acc_yhat in accs.items():
+        cutoffs_yhat = random_acc(len(acc_yhat))
+        auc_yhat = np.trapezoid(acc_yhat, cutoffs_yhat)
+        ax.plot(cutoffs_yhat, acc_yhat, label=f"{name}, {round(auc_yhat, 3)}", alpha=alpha)
         acc_yhats.append(acc_yhat)
 
-    acc_avg = average_curves(acc_yhats)
-    cutoffs_avg = random_acc(len(acc_avg))
-    ax.plot(cutoffs_avg, acc_avg, label="acc avg", color="green")
+    if plot_avg:
+        acc_avg = average_curves(acc_yhats)
+        cutoffs_avg = random_acc(len(acc_avg))
+        auc_avg = np.trapezoid(acc_avg, cutoffs_avg)
+        ax.plot(cutoffs_avg, acc_avg, label=f"acc avg, {round(auc_avg, 3)}", color="green")
 
-    ax.plot(cutoffs_avg, cutoffs_avg, "--", label="acc rand", color="red")
+    cutoffs_rand = random_acc(100)
+    auc_rand = np.trapezoid(cutoffs_rand, cutoffs_rand)
+    ax.plot(cutoffs_rand, cutoffs_rand, "--", label=f"acc rand, {round(auc_rand, 3)}", color="red")
 
     ax.set_xlabel("size of cutoff")
     ax.set_xlim(0, 1)
@@ -81,11 +77,125 @@ def plot_accs(df_ys: list[pd.DataFrame], df_yhats: list[pd.DataFrame], df_names:
     ax.legend(
         loc="lower right",
         # ncol=2,
-        bbox_to_anchor=(1.3, 0),
+        bbox_to_anchor=(1.5, 0),
         # fancybox=True,
-        handletextpad=0.1,
-        borderaxespad=0.1,
-        fontsize="small",
+        handletextpad=0.05,
+        borderaxespad=0.05,
+        fontsize=7,
     )
-    ax.set_aspect("equal")
-    return fig
+    ax.set_aspect("equal", anchor="SW")
+
+    if plot_avg:
+        return fig, acc_avg, round(auc_avg, 3)
+    else:
+        return fig, None, None
+
+
+
+
+
+
+
+import json
+from functools import lru_cache
+from pathlib import Path
+from typing import Any
+
+
+def load_json_data(json_path: Path):
+    print(json_path)
+    with open(json_path, "r") as file:
+        data = json.load(file)
+    return data
+
+
+class GTResults:
+
+    def __init__(self, results_raw: dict[str, Any]):
+        self.results_raw = results_raw
+    
+    @lru_cache
+    def get_ranking(self, net_type: str, net_name: str, protocol: int, p: float) -> list[Any]:
+        for result_raw in self.results_raw:
+            if (
+                result_raw["net_type"] == net_type and
+                result_raw["net_name"] == net_name and
+                result_raw["protocol"] == protocol and
+                result_raw["p"] == p
+            ):
+                return result_raw["seed_sets"][0]
+
+
+if __name__ == "__main__":
+
+    results_path = Path("data/iou_curves/20250317153249")
+    
+    results_jsons = list(results_path.glob("*.json"))
+    results_raw = {rj.stem: load_json_data(rj) for rj in results_jsons}
+    gt_results = GTResults(results_raw["ground_truth"])
+    del results_raw["ground_truth"]
+
+    all_accs = {}
+    for im_name, im_results in results_raw.items():
+        im_accs = {}
+        for im_result in im_results:
+            if im_result["net_type"] == im_result["net_name"]:
+                case_name = im_result["net_type"]
+            else:
+                case_name = f"{im_result["net_type"]}_{im_result["net_name"]}"
+            
+            # obtain cumulated accuracy of seed sets
+            gt_result = gt_results.get_ranking(
+                net_type=im_result["net_type"],
+                net_name=im_result["net_name"],
+                protocol=im_result["protocol"],
+                p=im_result["p"],
+            )
+            ca = cummulated_acc(arr_y=gt_result, arr_yhat=im_result["seed_sets"])
+
+            # save obtained curve in the dictionary and if needed create new fields there
+            if not im_accs.get(im_result["protocol"]):
+                im_accs[im_result["protocol"]] = {}    
+            if not im_accs[im_result["protocol"]].get(im_result["p"]):
+                im_accs[im_result["protocol"]][im_result["p"]] = {}
+            if im_accs[im_result["protocol"]][im_result["p"]].get(case_name):
+                raise ValueError(f"{case_name} already in parsed results!")
+            im_accs[im_result["protocol"]][im_result["p"]][case_name] = ca
+        
+        # when iterating through the method is over - save all results
+        all_accs[im_name] = im_accs
+
+    # now draw the results
+    pdf = PdfPages("pa_artificial.pdf")
+    avg_accs = {}
+    for im_name, im_dict in all_accs.items():
+        for protocol, p_dict in im_dict.items():
+            for p, pp_dict in p_dict.items():
+
+                # plot for all networks for given params
+                fig, acc_avg, auc_avg = plot_accs(accs=pp_dict)
+                fig.suptitle(f"im_name: {im_name}, protocol: {protocol}, p: {p}, auc: {auc_avg}")
+                fig.tight_layout()
+                fig.savefig(pdf, format="pdf")
+                plt.close(fig)
+
+                # save average acc in to plot it again against all methods
+                if not avg_accs.get(protocol):
+                    avg_accs[protocol] = {}
+                if not avg_accs[protocol].get(p):
+                    avg_accs[protocol][p] = {}
+                if avg_accs[protocol][p].get(im_name):
+                    raise ValueError(f"{im_name} already in parsed results!")
+                avg_accs[protocol][p][im_name] = acc_avg # {"acc": acc_avg, "auc": auc_avg}
+
+    # draw average curves for each infmax method on a single canvas
+    for protocol, p_dict in avg_accs.items():
+        for p, pp_dict in p_dict.items():
+            # for im_name, im_dict in pp_dict.items():
+            fig, _, _ = plot_accs(accs=pp_dict, plot_avg=False)
+            fig.suptitle(f"im_name: {im_name}, protocol: {protocol}, p: {p}")
+            fig.tight_layout()
+            fig.savefig(pdf, format="pdf")
+            plt.close(fig)
+
+    pdf.close()
